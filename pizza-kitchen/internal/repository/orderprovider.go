9fd00b6869f6483d2646778bd987d4f90feea1f5
Nikita <muravev2004@list.ru>
2024-03-23 23:57:49 +0300

    #3

--- pizza-kitchen/internal/repository/orderprovider.go
+++ pizza-kitchen/internal/repository/orderprovider.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"fmt"
-	"strconv"
 
 	"github.com/eeQuillibrium/pizza-kitchen/internal/domain/models"
 	"github.com/redis/go-redis/v9"
@@ -25,43 +24,10 @@ func (r *OPRepo) StoreOrder(
 	ctx context.Context,
 	order *models.Order,
 ) error {
-	var err error
-	orderKey := fmt.Sprintf("order:%d", order.OrderId)
-
-	if err = r.storeOrder(ctx, order, orderKey); err != nil {
-		return err
-	}
-
-	userKey := fmt.Sprintf("user:%d", order.UserId)
-
-	var last int = -1
-
-	lastStr := r.rClient.HGetAll(ctx, userKey).Val()["last"]
-	if lastStr != "" {
-		last, err = strconv.Atoi(lastStr)
-		if err != nil {
-			return err
-		}
-	}
-
-	if err := r.rClient.HMSet(
-		ctx, userKey,
-		fmt.Sprintf("order:%d", last+1), orderKey,
-		"last", last+1).
-		Err(); err != nil {
-		return err
-	}
-
-	return nil
-}
-func (r *OPRepo) storeOrder(
-	ctx context.Context,
-	order *models.Order,
-	orderKey string,
-) error {
-	if err := r.rClient.HMSet(
+	orderkey := fmt.Sprintf("order:%d", order.OrderId)
+	if err := r.rClient.HSet(
 		ctx,
-		orderKey,
+		orderkey,
 		"orderid", order.OrderId,
 		"userid", order.UserId,
 		"price", order.Price,
@@ -72,7 +38,7 @@ func (r *OPRepo) storeOrder(
 	}
 
 	for i := 0; i < len(order.Units); i++ {
-		if err := r.rClient.HMSet(ctx, orderKey,
+		if err := r.rClient.HSet(ctx, orderkey,
 			fmt.Sprintf("unitnum%d", i), order.Units[i].Unitnum,
 			fmt.Sprintf("piece%d", i), order.Units[i].Piece).
 			Err(); err != nil {
